a36c2dd76814d6fd5799273f7239bd1a2a9b2b71
Кундуз Рахатова <krahatova@mpz.kg>
2022-02-02 20:09:38 +0600

    inicial

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const config = require("config")
+const PORT = config.get("port")
+
+async function start() {
+    try {
+       await mongoose.connect(config.get('mongoURL'))
+    } catch (e) {
+        console.log("Server error", e.message)
+        process.exit(1)
+    }
+}
+
+start()
+app.listen(PORT, () => console.log(`App has been started on port ${PORT}`))
